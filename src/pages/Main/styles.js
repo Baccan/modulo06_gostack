56dc09a7c3c0ded6426c6191b5b749e7a0264556
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-16 16:04:47 -0300

    Estilizando formulário

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 // Sempre se utiliza o nome da tag, e não o elemento
 export const Container = styled.View`
@@ -9,4 +10,31 @@ export const Container = styled.View`
   /* Text {} */
 
   /* Não se pode criar estilos globais */
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 1px;
+  border-color: #eee;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  justify-content: center;
+  align-items: center;
+  background: #7159c1;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
 `;
