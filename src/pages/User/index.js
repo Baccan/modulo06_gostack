c84c9007c244324097ddc8b8924afb67f94f8864
Gustavo Baccan Gomes <gustavo.gomes@butantan.gov.br>
2019-07-17 09:27:28 -0300

    Buscando favoritos de usuarios no github

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User({ navigation }) {
-  // console.tron.log(props);
-  return <View />;
+export default class User extends Component {
+  // uma propriedade não cosegue acessar componentes do this
+  // se tirar o static não irá funcionar pois é preciso passa-lo como static para o componente de navegação
+  // para isso, utilizamos uma função que retorna um objeto () => ({})
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
+
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+    return <View />;
+  }
 }
